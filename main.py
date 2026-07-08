82019c6331ba19c62558e77cad473c9822e1cb17
ycolinta <ycolinta@uvm.edu>
2026-07-08 15:15:31 -0400

    Updated AI's prompt instructions and have labeled 10 images with parent assessment

--- main.py
+++ main.py
@@ -18,12 +18,26 @@ client = genai.Client(api_key=api_key)
 
 # print(api_key is not None)
 
-uploaded_file = client.files.upload(file="images/test_img1.jpeg")
+uploaded_file = client.files.upload(file="images/image03.jpeg")
 
 interaction = client.interactions.create(
     model="gemini-2.5-flash",
     input=[
-        {"type": "text", "text": "Caption this image."},
+        {"type": "text", "text": """
+        You are assisting with the assessment of infant sleep environments for a computer science research project.
+        Analyze the infant sleep environment shown in this image.
+        Based only on the visible information, determine whether the image shows:
+        
+        - No apparent safety concerns.
+        - Possible safety concerns.
+        - Serious safety concerns.
+        
+        Exactly one of these categories must be selected as true. The other two categories must be false.
+        
+       Briefly explain the observations that led to your assessment in the explanation field.
+
+        Return each of the responses as JSON.
+        """},
         {
             "type": "image",
             "uri": uploaded_file.uri,
